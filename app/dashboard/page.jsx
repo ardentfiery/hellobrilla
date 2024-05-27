48bbfe444a175274e936ea29408bc9a6225c1128
undef125 <076bct038@ioepc.edu.np>
2024-05-27 22:26:58 +0545

    almost done

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Left from "@/components/dashboard/Left";
 import axios from "@/app/api/axiosintercepter";
 import Right from "@/components/dashboard/Right";
