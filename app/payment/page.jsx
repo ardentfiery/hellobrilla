39c2572ab2911aeea47b7413f2154d652102a8b0
undef125 <076bct038@ioepc.edu.np>
2024-06-18 22:34:12 +0545

    fixing payment page checking for already sent request

--- app/payment/page.jsx
+++ app/payment/page.jsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
 const page = () => {
   const router = useRouter();
+  const [isRequestAlreadySent, setisRequestAlreadySent] = useState(false);
 
   const [brillaDetails, setbrillaDetails] = useState();
   const paymentFunc = async () => {
@@ -23,7 +24,9 @@ const page = () => {
           {
             product: {
               name: "Brilla system ",
-              price: brillaDetails?.discountPrice ? brillaDetails?.discountPrice: brillaDetails.originalPrice ,
+              price: brillaDetails?.discountPrice
+                ? brillaDetails?.discountPrice
+                : brillaDetails.originalPrice,
               image:
                 "https://res.cloudinary.com/do7fzmdl3/image/upload/q_auto:low/brilla_mfs7nk.png",
               // eventId:currentEvent._id
@@ -51,7 +54,7 @@ const page = () => {
       await axios.post("/user/addverificationrequestnotstripe");
       toast.dismiss(toastId);
       toast.success("Verification request successfully sent");
-      router.push("/buysuccess")
+      router.push("/buysuccess");
     } catch (error) {
       toast.dismiss(toastId);
       toast.error("Verification request failed to sent");
@@ -67,88 +70,109 @@ const page = () => {
       console.log(error);
     }
   };
+  const alreadyRequestSentCheck = async () => {
+    try {
+      const datarecieved = await axios.get("/user/alreadyrequestsentcheck");
+      setisRequestAlreadySent(true);
+    } catch (error) {
+      console.log(error);
+      setisRequestAlreadySent(false);
+    }
+  };
 
   useEffect(() => {
     getBrillDetails();
+    alreadyRequestSentCheck();
   }, []);
 
   return (
-    <div className="flex flex-col gap-[4rem] mt-[2rem] ">
-      <div className="book flex flex-col font-bold text-3xl md:text-5xl items-center">
-        <p>Buy System In Order To Access Dashboard</p>
-        <div className="flex ">
-          <div className="h-[4px] w-[20rem] bg-[#664198] "></div>
-          <div className="h-[4px] w-[10rem] bg-[#664198] ml-[30px]"></div>
-        </div>
-      </div>
-      <div className=" flex justify-center gap-[4rem] w-[100vw] items-center">
-        <div className="flex flex-col gap-[4rem] h-[30rem] py-2  justify-between">
-          <div className="book ml-[-10rem]">
-            <img
-              className="md:h-[20rem] "
-              src={brillaDetails?.brillaImage}
-              onError={(e) => {
-                e.target.onerror = null; // Prevent looping
-                e.target.src = "/landing/book.png";
-              }}
-              alt=""
-            />
-            <div className="flex justify-center font-semibold md:text-3xl text-2xl">
-              <div>
-                <p className="text-3xl font-semibold text-center md:text-left">
-                  COSTO:
-                  {brillaDetails?.brillaPrice ? (
-                    brillaDetails?.brillaPrice
-                  ) : brillaDetails?.discountPrice ? (
-                    <>
-                      {" "}
-                      <span className="text-3xl font-light line-through">
-                        ${brillaDetails?.originalPrice}
-                      </span>{" "}
-                      ${brillaDetails?.discountPrice}
-                    </>
-                  ) : (
-                    <p>{brillaDetails?.originalPrice}$</p>
-                  )}
+    <>
+      {isRequestAlreadySent ? (
+        <div className="flex flex-col gap-[4rem] mt-[2rem] ">
+          <div className="book flex flex-col font-bold text-3xl md:text-5xl items-center">
+            <p>Buy System In Order To Access Dashboard</p>
+            <div className="flex ">
+              <div className="h-[4px] w-[20rem] bg-[#664198] "></div>
+              <div className="h-[4px] w-[10rem] bg-[#664198] ml-[30px]"></div>
+            </div>
+          </div>
+          <div className=" flex justify-center gap-[4rem] w-[100vw] items-center">
+            <div className="flex flex-col gap-[4rem] h-[30rem] py-2  justify-between">
+              <div className="book ml-[-10rem]">
+                <img
+                  className="md:h-[20rem] "
+                  src={brillaDetails?.brillaImage}
+                  onError={(e) => {
+                    e.target.onerror = null; // Prevent looping
+                    e.target.src = "/landing/book.png";
+                  }}
+                  alt=""
+                />
+                <div className="flex justify-center font-semibold md:text-3xl text-2xl">
+                  <div>
+                    <p className="text-3xl font-semibold text-center md:text-left">
+                      COSTO:
+                      {brillaDetails?.brillaPrice ? (
+                        brillaDetails?.brillaPrice
+                      ) : brillaDetails?.discountPrice ? (
+                        <>
+                          {" "}
+                          <span className="text-3xl font-light line-through">
+                            ${brillaDetails?.originalPrice}
+                          </span>{" "}
+                          ${brillaDetails?.discountPrice}
+                        </>
+                      ) : (
+                        <p>{brillaDetails?.originalPrice}$</p>
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div
+                  onClick={() => {
+                    paymentFunc();
+                  }}
+                  className="flex justify-center mt-2"
+                >
+                  <button className="border-[#664198] border-[1px] py-2 px-[6rem] font-bold text-[1.2rem] rounded hover:bg-[#664198] hover:text-white transition-all ease-in-out duration-300">
+                    Purchase
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="w-[30rem] h-[30rem] bg-slate-300 rounded flex flex-col justify-between py-2">
+                <p className="font-bold text-[2rem] text-center">
+                  Already done the payment by other means?
                 </p>
+                <p className="font-lightbold text-[1.2rem] text-center">
+                  Click the button below to send verification request to admins.
+                </p>
+                <div
+                  onClick={() => {
+                    sendVerificationRequest();
+                  }}
+                  className="flex justify-center mt-2"
+                >
+                  <button className="border-[#664198] border-[1px] py-2 px-[6rem] font-bold text-[1.2rem] rounded hover:bg-[#664198] hover:text-white transition-all ease-in-out duration-300">
+                    Send Verification Request
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-          <div className="">
-            <div
-              onClick={() => {
-                paymentFunc();
-              }}
-              className="flex justify-center mt-2"
-            >
-              <button className="border-[#664198] border-[1px] py-2 px-[6rem] font-bold text-[1.2rem] rounded hover:bg-[#664198] hover:text-white transition-all ease-in-out duration-300">
-                Purchase
-              </button>
-            </div>
-          </div>
         </div>
-        <div>
-          <div className="w-[30rem] h-[30rem] bg-slate-300 rounded flex flex-col justify-between py-2">
-            <p className="font-bold text-[2rem] text-center">
-              Already done the payment by other means?
-            </p>
-            <p className="font-lightbold text-[1.2rem] text-center">
-              Click the button below to send verification request to admins.
-            </p>
-            <div
-              onClick={() => {
-                sendVerificationRequest();
-              }}
-              className="flex justify-center mt-2"
-            >
-              <button className="border-[#664198] border-[1px] py-2 px-[6rem] font-bold text-[1.2rem] rounded hover:bg-[#664198] hover:text-white transition-all ease-in-out duration-300">
-                Send Verification Request
-              </button>
-            </div>
-          </div>
+      ) : (
+        <div className="h-[100vh] w-[100vw] max-w-[100%] flex justify-center items-center ">
+          <p className="text-[2rem] font-semibold">
+            Your request has already been sent please wait for verification by
+            our admin and check your mail!
+          </p>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
