d7cf929addc41d2270cf64d5ff2b627088ad7fab
undef125 <076bct038@ioepc.edu.np>
2024-07-05 20:42:12 +0545

    try

--- app/page.jsx
+++ app/page.jsx
@@ -58,25 +58,22 @@ const Home = () => {
     try {
       const isAuthorized = await axios.get("/user/authorize");
       console.log("---------------- not authorized --------------------");
-      console.log("not authorized", isAuthorized);
       try {
         const data = await axios.get("/user/userotpverified");
-        console.log("otp not verified", data);
         console.log("---------------- otp not verified --------------------");
         try {
           const data = await axios.get("/user/paidforbrilla");
-          console.log("not paid for brilla", data);
           console.log(
             "---------------- not paid for brilla --------------------"
           );
 
           router.push("/dashboard");
         } catch (error) {
-          setisLoading(false);
+          // setisLoading(false);
           router.push("/payment");
         }
       } catch (error) {
-        setisLoading(false);
+        // setisLoading(false);
         router.push("/verifyotp");
       }
     } catch (error) {
