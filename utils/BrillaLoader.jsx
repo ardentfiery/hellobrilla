ea6e7bcbe4f2d66aaf638b1b523642d7964c60b3
undef125 <076bct038@ioepc.edu.np>
2024-07-05 22:15:06 +0545

    almost done

--- utils/BrillaLoader.jsx
+++ utils/BrillaLoader.jsx
@@ -3,10 +3,15 @@ import React from "react";
 const BrillaLoader = () => {
   return (
     <div className="absolute z-50 top-0 left-0 h-[100vh] w-[100vw] bg-white flex justify-center items-center ">
-      <div className="w-[30rem]">
-        <img src="/landing/logo.png" className="w-[30rem] h-auto  " style={{
-            animation: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite"
-        }} alt="" />
+      <div className="w-[70vw] md:w-[30rem]">
+        <img
+          src="/landing/logo.png"
+          className="w-full h-auto  "
+          style={{
+            animation: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+          }}
+          alt=""
+        />
       </div>
     </div>
   );
